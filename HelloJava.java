c2ad6864dcde101a2c60e880da26d7264fe68145
kaiwen1998 <kai_wen1998@yahoo.com>
2022-04-13 21:41:34 -0400

    Update HelloJava.java

--- HelloJava.java
+++ HelloJava.java
@@ -2,5 +2,12 @@ public class JavaHelloWorldProgram {
 
 	public static void main(String args[]){
 		System.out.println("Hello World");
+		String str = "111 22 2";
+		countSpaces(str);
+	}
+	
+	static countSpaces(String string) {
+		int spaceCount = INPUT_STRING.replaceAll("[^ ]", "").length();
+		System.out.printlin(spaceCount);
 	}
 }
